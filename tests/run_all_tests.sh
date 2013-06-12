8f4cb38f04b6e461e0d8bd0d70e8ec3eaa79d0a5
Sam Thompson <georgedorn@gmail.com>
2013-06-12 14:22:26 -0700

    Don't wait for input in a travis build.

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -32,7 +32,7 @@ for type in $TYPES; do
         django-admin.py test ${type}_resource --settings=settings_$type
         continue
     elif [ $type == 'gis' ]; then
-        sudo -u postgres createdb -T template_postgis tastypie.db
+        sudo createdb -T template_postgis tastypie.db
     fi
 
     django-admin.py test $type --settings=settings_$type
